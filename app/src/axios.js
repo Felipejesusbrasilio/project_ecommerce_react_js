16ad91468a0a178778457edc9b97d36fe67d180f
FELIPE DE JESUS BRASILIO DA COSTA <69780275+Felipejesusbrasilio@users.noreply.github.com>
2025-09-02 08:26:52 -0300

    Update axios.js

--- app/src/axios.js
+++ app/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://project-ecommerce-react-js-xfbp.vercel.app/"
+  baseURL: "https://api-node-js-three.vercel.app/"
 });
 
 export default api;
